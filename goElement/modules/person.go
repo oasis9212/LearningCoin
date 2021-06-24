894f342f244bbed3c101d672091a4e48fe184980
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-06-24 23:37:23 +0900

    5일차 특별한건 없었다 .  포인터의 활용 변수참조와 주소참조.

--- goElement/modules/person.go
+++ goElement/modules/person.go
@@ -5,7 +5,12 @@ type Person struct {
 	age  int
 }
 
-func (p Person) SeDetails(name string, age int) {
+func (p *Person) SeDetails(name string, age int) { // *Person 참조 표시 해야  주소 참조를 해서 변경이 가능핟.
 	p.name = name
 	p.age = age
+
+}
+
+func (p Person) Getname() string {
+	return p.name
 }
