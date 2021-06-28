b3df9dd8d5e9ee3c531d09a0bd55f161ff410e6e
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-06-28 23:11:14 +0900

    6일차 블록체인의 구성및 정의 제네시스 블록 완성.

--- main.go
+++ main.go
@@ -12,17 +12,34 @@ type block struct {
 }
 
 type blockchain struct {
-	block []block
+	blocks []block
+}
+
+func (b *blockchain) getLastHash() string {
+	if len(b.blocks) > 0 {
+		return b.blocks[len(b.blocks)-1].hash
+	}
+	return ""
+} // 마지막 블록에 대한 해쉬값가져오기.
+
+func (b *blockchain) addBlock(data string) {
+	newBlock := block{data, "", b.getLastHash()}
+	hash := sha256.Sum256([]byte(newBlock.data + newBlock.prevHash))
+	newBlock.hash = fmt.Sprintf("%x", hash)
+	b.blocks = append(b.blocks, newBlock)
+}
+func (b *blockchain) listBlocks() {
+	for _, block := range b.blocks {
+		fmt.Printf("data is %s\n", block.data)
+		fmt.Printf("data hash is %s\n", block.hash)
+		fmt.Printf("data prevhash is %s\n", block.prevHash)
+	}
 }
 
 func main() {
-	// go언어의 string 값은 불변하다.
-
-	genesisBlcok := block{"Genesis Block", "", ""}
-	//genesisBlcok.hash=  hashfunction(genesisBlcok.data+genesisBlcok.prevHash)   // SHA-256 해쉬 함수를 활용한다.
-	hash := sha256.Sum256([]byte(genesisBlcok.data + genesisBlcok.prevHash)) //  SHA256 이미 구현되어있다.
-	hexHash := fmt.Sprintf("%x", hash)                                       // string 값으로 변환한다.
-	genesisBlcok.hash = hexHash                                              // 제네시스 블록 완성
-
-	secondBlock := block{"Second Blocks", "", genesisBlcok.hash}
+	chain := blockchain{}
+	chain.addBlock("Genesis Block")
+	chain.addBlock("Second Block")
+	chain.addBlock("Third Block")
+	chain.listBlocks()
 }
