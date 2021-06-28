53570d3de39beaa1d55fa724f57b9b0e3d29b369
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-06-28 22:48:17 +0900

    6일차 블록체인의 구성및 정의 제네시스 블록 완성.

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-	"ralo/goElement/modules"
 )
 
+type block struct {
+	data     string //  저장할 내용
+	hash     string // 저장할 해쉬 내용.
+	prevHash string
+}
+
+type blockchain struct {
+	block []block
+}
+
 func main() {
-	fmt.Println("it works")
-	nako := modules.Person{}
+	// go언어의 string 값은 불변하다.
 
-	fmt.Println(nako)
-	nako.SeDetails("나코", 21)
-	fmt.Println(nako)
+	genesisBlcok := block{"Genesis Block", "", ""}
+	//genesisBlcok.hash=  hashfunction(genesisBlcok.data+genesisBlcok.prevHash)   // SHA-256 해쉬 함수를 활용한다.
+	hash := sha256.Sum256([]byte(genesisBlcok.data + genesisBlcok.prevHash)) //  SHA256 이미 구현되어있다.
+	hexHash := fmt.Sprintf("%x", hash)                                       // string 값으로 변환한다.
+	genesisBlcok.hash = hexHash                                              // 제네시스 블록 완성
 
+	secondBlock := block{"Second Blocks", "", genesisBlcok.hash}
 }
