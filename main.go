d02795da6b266acbd8f45a7b92ea2e3dd171352a
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-06-30 23:54:53 +0900

    8일차 블록체인 html 구현 2번째 차

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"ralo/blockchain"
+	"text/template"
 )
 
-const port string = ":4000"
+const (
+	port        string = ":4000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type HomeData struct {
 	PageTitle string
@@ -17,14 +22,28 @@ type HomeData struct {
 
 //template.ParseFiles("templates/home.html")
 func home(rw http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.gohtml")) // template.Must 자동으로 에러처리를 해주는 방식.
 	data := HomeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	tmpl.Execute(rw, data)
+	templates.ExecuteTemplate(rw, "home", data)
 
 }
 
+func add(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(rw, "add", nil)
+	case "POST":
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
 func main() {
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
