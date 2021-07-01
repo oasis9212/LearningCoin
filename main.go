6297a3cb837c0016cbc5719d0516f5518cfe305b
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-07-01 22:37:06 +0900

    9일차 html 블록체인 구현 완성.

--- main.go
+++ main.go
@@ -1,49 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"ralo/blockchain"
-	"text/template"
+	"ralo/explorer"
 )
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type HomeData struct {
-	PageTitle string
-	Blocks    []*blockchain.Block
-}
-
-//template.ParseFiles("templates/home.html")
-func home(rw http.ResponseWriter, r *http.Request) {
-	data := HomeData{"Home", blockchain.GetBlockChain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockChain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home)
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+
+	explorer.Start()
 }
