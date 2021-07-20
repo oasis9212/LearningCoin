0b0b3f5eef8848e2ff909ae64b2510ae6f81f03d
oasis9212@gmail.com <48095797+oasis9212@users.noreply.github.com>
2021-07-20 21:57:36 +0900

    11일차 CLI 완성.

---
+++ cli/cli.go
@@ -0,0 +1,45 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"ralo/explorer"
+	"ralo/rest"
+)
+
+func usage() {
+	fmt.Printf("useless coin\n\n")
+	fmt.Printf("Please use the following flags:\n\n")
+	fmt.Printf("-port: Set the Port of the server \n")
+	fmt.Printf("-mode:  chosse 'html' and 'rest' \n")
+	os.Exit(0)
+
+}
+
+func Start() {
+	fmt.Println(os.Args)
+	// Array[2:]  3번째 배열부터 끝까지 를 뜻한다.
+	// os.Args 는 길이가 1인 전재로 시작한다 인자값 go run main.go 자체가 길이가 1일다.
+	//go explorer.Start(3000)
+	if len(os.Args) < 2 {
+		usage()
+	}
+
+	port := flag.Int("port", 4000, "Set Port Server")
+
+	mode := flag.String("mode", "rest", "chosse 'html' and 'rest'")
+
+	flag.Parse()
+
+	switch *mode {
+	case "rest":
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
+	default:
+		usage()
+	}
+
+	fmt.Println(*port, *mode)
+}
--- main.go
+++ main.go
@@ -1,45 +1,8 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-)
-
-func usage() {
-	fmt.Printf("useless coin\n\n")
-	fmt.Printf("Please use the following commands:\n\n")
-	fmt.Printf("explorer:   this starts the HTML explorer\n")
-	fmt.Printf("rest:   this starts the REST API (recommended)\n")
-	os.Exit(0)
-
-}
+import "ralo/cli"
 
 func main() {
 
-	fmt.Println(os.Args)
-	// Array[2:]  3번째 배열부터 끝까지 를 뜻한다.
-	// os.Args 는 길이가 1인 전재로 시작한다 인자값 go run main.go 자체가 길이가 1일다.
-	//go explorer.Start(3000)
-	if len(os.Args) < 2 {
-		usage()
-	}
-
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-
-	portFlag := rest.Int("port", 4000, "Sets the port of the server ")
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("start Explorer")
-	case "rest":
-		rest.Parse(os.Args[2:])
-	default:
-		usage()
-	}
-
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
-	fmt.Println(*portFlag)
+	cli.Start()
 }
